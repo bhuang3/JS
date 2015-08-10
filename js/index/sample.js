3d89bfb63ee949283baaa578e94e8cfee4fcc703
bhuang3 <bhuang1228@gmail.com>
2015-08-10 00:31:49 -0400

    Rename

---
+++ js/index/sample.js
@@ -0,0 +1,376 @@
+/**
+ * Created by bhuang3 on 7/28/15.
+ */
+var sample = [
+    {
+        title: "第一例",
+        sample: [{
+            text: "控制Div属性",
+            href: "sample1/01.html"
+        },
+        {
+            text: "网页换肤",
+            href: "sample1/02.html"
+        },
+        {
+            text: "函数接收参数并弹出",
+            href: "sample1/03.html"
+        },
+        {
+            text: "用循环将三个Div变成红色",
+            href: "sample1/04.html"
+        },
+        {
+            text: "鼠标移入/移出改变样式",
+            href: "sample1/05.html"
+        },
+        {
+            text: "记住密码提示框",
+            href: "sample1/06.html"
+        }]
+    },
+    {
+        title: "第二例",
+        sample: [{
+            text: "百度输入法",
+            href: "sample2/01.html"
+        },
+        {
+            text: "点击Div，显示其innerHTML",
+            href: "sample2/02.html"
+        },
+        {
+            text: "求出数组中所有数字的和",
+            href: "sample2/03.html"
+        },
+        {
+            text: "弹出层效果",
+            href: "sample2/04.html"
+        },
+        {
+            text: "函数传参，改变Div任意属性的值",
+            href: "sample2/05.html"
+        },
+        {
+            text: "图片列表：鼠标移入/移出改变图片透明度",
+            href: "sample2/06.html"
+        },
+        {
+            text: "简易选项卡",
+            href: "sample2/07.html"
+        },
+        {
+            text: "简易JS年历",
+            href: "sample2/08.html"
+        },
+        {
+            text: "单一按钮显示/隐藏一播放列表收缩展开",
+            href: "sample2/09.html"
+        },
+        {
+            text: "提示框效果",
+            href: "sample2/10.html"
+        },
+        {
+            text: "鼠标移过，修改图片路径",
+            href: "sample2/11.html"
+        },
+        {
+            text: "复选框（checkbox）全选/全不选/返选",
+            href: "sample2/12.html"
+        }]
+    },
+    {
+        title: "第三例",
+        sample: [{
+            text: "用typeof查看数据类型",
+            href: "sample3/01.html"
+        },
+        {
+            text: "用parseInt解析数字，并求和",
+            href: "sample3/02.html"
+        },
+        {
+            text: "累加按钮，自加1",
+            href: "sample3/03.html"
+        },
+        {
+            text: "输入两个数字，比较大小",
+            href: "sample3/04.html"
+        },
+        {
+            text: "页面加载后累加，自加1",
+            href: "sample3/05.html"
+        },
+        {
+            text: "判断数字是否为两位数",
+            href: "sample3/06.html"
+        },
+        {
+            text: "网页计算器",
+            href: "sample3/07.html"
+        },
+        {
+            text: "简易网页时钟",
+            href: "sample3/08.html"
+        },
+        {
+            text: "倒计时时钟（100秒）",
+            href: "sample3/09.html"
+        }]
+    },
+    {
+        title: "第四例",
+        sample: [{
+            text: "setTimeout应用",
+            href: "sample4/01.html"
+        },
+        {
+            text: "自动播放一幻灯片效果",
+            href: "sample4/02.html"
+        },
+        {
+            text: "自动改变方向一幻灯片效果",
+            href: "sample4/03.html"
+        },
+        {
+            text: "agruments应用一求出函数参数的总合",
+            href: "sample4/04.html"
+        },
+        {
+            text: "css函数一设置/读取对象的属性",
+            href: "sample4/05.html"
+        },
+        {
+            text: "当前输入框高亮显示",
+            href: "sample4/06.html"
+        },
+        {
+            text: "数组练习：各种数组方法的使用",
+            href: "sample4/07.html"
+        },
+        {
+            text: "事件练习：封装兼容性添加、删除事件的函数",
+            href: "sample4/08.html"
+        },
+        {
+            text: "星级评分系统",
+            href: "sample4/09.html"
+        }]
+    },
+    {
+        title: "第五例",
+        sample: [{
+            text: "模拟select控件",
+            href: "sample5/01.html"
+        },
+        {
+            text: "点击页面，显示单击的坐标",
+            href: "sample5/02.html"
+        },
+        {
+            text: "用户按下键盘，显示keyCode",
+            href: "sample5/03.html"
+        },
+        {
+            text: "阻止右键菜单（阻止默认事件）",
+            href: "sample5/04.html"
+        },
+        {
+            text: "跟随鼠标移动（大图展示）",
+            href: "sample5/05.html"
+        },
+        {
+            text: "自定义右键菜单",
+            href: "sample5/06.html"
+        },
+        {
+            text: "用键盘控制Div",
+            href: "sample5/07.html"
+        },
+        {
+            text: "Div闪烁",
+            href: "sample5/08.html"
+        }]
+    },
+    {
+        title: "第六例",
+        sample: [{
+            text: "完美拖拽",
+            href: "sample6/01.html"
+        },
+        {
+            text: "仿腾讯微博效果",
+            href: "sample6/02.html"
+        },
+        {
+            text: "自定义多级右键菜单",
+            href: "sample6/03.html"
+        }]
+    },
+    {
+        title: "第七例",
+        sample: [{
+            text: "自动轮播广告（缓冲效果）",
+            href: "sample7/01.html"
+        },
+        {
+            text: "图片切换（Loading效果）",
+            href: "sample7/02.html"
+        },
+        {
+            text: "移动效果（按轨迹移动）",
+            href: "sample7/03.html"
+        }],
+    },
+    {
+        title: "第八例",
+        sample: [{
+            text: "iPhone手机解锁效果",
+            href: "sample8/01.html"
+        },
+        {
+            text: "自定义滚动条",
+            href: "sample8/02.html"
+        },
+        {
+            text: "拖拽一Clone",
+            href: "sample8/03.html"
+        },
+        {
+            text: "拖拽并可以改变大小",
+            href: "sample8/04.html"
+        }]
+    },
+    {
+        title: "第九例",
+        sample: [{
+            text: "仿苹果电脑任务栏菜单",
+            href: "sample9/01.html"
+        },
+        {
+            text: "拼图小游戏",
+            href: "sample9/02.html"
+        },
+        {
+            text: "模拟表单控件",
+            href: "sample9/03.html"
+        }]
+    },
+    {
+        title: "第十例",
+        sample: [{
+            text: "为数组添加方法（求和，最大值）",
+            href: "sample10/01.html"
+        },
+        {
+            text: "自动轮播广告（面向对象版）",
+            href: "sample10/02.html"
+        },
+        {
+            text: "运动框架（面向对象版）",
+            href: "sample10/03.html"
+        },
+        {
+            text: "照片墙一多实例演示（面向对象版）",
+            href: "sample10/04.html"
+        },
+        {
+            text: "拖拽库（面向对象版）",
+            href: "sample10/05.html"
+        },
+        {
+            text: "无缝滚动（面向对象版）",
+            href: "sample10/06.html"
+        },
+        {
+            text: "延时加载（面向对象版）",
+            href: "sample10/07.html"
+        },
+        {
+            text: "仿腾讯游戏《英雄杀》官网Flash效果",
+            href: "sample10/08.html"
+        },
+        {
+            text: "百度有啊通栏展示效果",
+            href: "sample10/09.html"
+        },
+        {
+            text: "放烟花效果（面向对象版）",
+            href: "sample10/10.html"
+        },
+        {
+            text: "《穿越火线》官网导航菜单",
+            href: "sample10/11.html"
+        },
+        {
+            text: "《百度风云榜》TAB切换",
+            href: "sample10/12.html"
+        },
+        {
+            text: "IBM官网导航条效果",
+            href: "sample10/13.html"
+        },
+        {
+            text: "面试题-动态生成表格",
+            href: "sample10/14.html"
+        }]
+    },
+    {
+        title: "项目实例",
+        sample: [{
+            text: "眼镜在线试戴",
+            href: "eyeglass"
+        },
+        {
+            text: "日历组件",
+            href: "calendar"
+        },
+        {
+            text: "淘宝网-迷你登录",
+            href: "taobao-login"
+        },
+        {
+            text: "仿Google+ 游戏频道焦点图效果",
+            href: "google"
+        }]
+    },
+    {
+        title: "YUI实例",
+        sample: [{
+            text: "淘宝旅行通用日历组件",
+            href: "calendar/trip-calendar"
+        },
+        {
+            text: "酒店价格日历",
+            href: "calendar/price-calendar"
+        }]
+    },
+    {
+        title: "KISSY实例",
+        sample: [{
+            text: "KISSY版-酒店价格日历",
+            href: "calendar/hotel-price-calendar"
+        },
+        {
+            text: "KISSY版-图片浏览组件",
+            href: "lightbox"
+        },
+        {
+            text: "酒店地图交通与周边",
+            href: "rich-map"
+        },
+        {
+            text: "多SKU酒店价格中心",
+            href: "price-center/hotel"
+        },
+        {
+            text: "多SKU门票价格中心",
+            href: "price-center/ticket"
+        },
+        {
+            text: "新版-多SKU门票价格中心",
+            href: "price-center/newticket"
+        }]
+    }
+];
